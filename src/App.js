e374fc6fa97b74a6af15769c89cef082b90b78ef
Ben Fellows <benjamin.fellows@bbc.co.uk>
2019-12-12 15:18:28 +0000

    render props and children

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Toggle from "./ToggleRenderPropsChildren.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Toggle>
+          {({ on, toggle }) => (
+            <div>
+              {on && <h1>Show Me</h1>}
+              <button onClick={toggle}>Show / Hide</button>
+            </div>
+          )}
+        </Toggle>
+
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              {on && <h1 on={on}>Show Me</h1>}
+              <button onClick={toggle}>Show / Hide</button>
+            </Fragment>
+          )}
+        </Toggle>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Portal.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import ReactDOM from "react-dom";
+
+const portalRoot = document.getElementById("portal");
+export default class Portal extends Component {
+  constructor() {
+    super();
+    this.el = document.createElement("div");
+  }
+  render() {
+    const { children } = this.props;
+    return ReactDOM.createPortal(children, this.el);
+  }
+}
